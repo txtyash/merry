914ed2924b8b49d38edca695e434aa74b82dfec4
Yash Shinde <shinde27yash@gmail.com>
2024-03-27 22:43:10 +0530

    experimenting

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,59 @@
-use mewe::{search, Search};
-use slint::{PhysicalSize, SharedString};
-
+use reqwest::get;
+use serde_derive::Deserialize;
+use serde_derive::Serialize;
+use slint::PhysicalSize;
+use slint::VecModel;
+use std::error::Error;
 slint::include_modules!();
+
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Defs {
+    pub list: Vec<Def>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Def {
+    pub word: String,
+    pub definition: String,
+    pub example: String,
+}
+
 #[tokio::main]
 async fn main() {
-    let ui = MerryWindow::new().unwrap();
+    let ui = MainWindow::new().unwrap();
     let weak = ui.as_weak();
+    ui.window().set_size(PhysicalSize::new(600, 400));
+    ui.run().unwrap();
     ui.on_search(move |text| {
-        dbg!("clicked!");
+        dbg!("called");
         let ui = weak.unwrap();
-        let query = text.to_string();
-        dbg!(&query);
         slint::spawn_local(async move {
-            dbg!(&query);
-            let response = match search(query).await {
-                Ok(Search::Definition(d)) => dbg!(d),
-                Ok(Search::Suggestions(s)) => dbg!(s),
-                Err(e) => dbg!(e.to_string()),
-            };
-            dbg!(&response);
-            ui.set_response(SharedString::from(response));
+            let defs: Defs = find(text.into()).await.unwrap();
+            let defs: Vec<Definition> = defs
+                .list
+                .into_iter()
+                .map(|def| Definition {
+                    word: def.word.into(),
+                    definition: def.definition.into(),
+                    example: def.example.into(),
+                })
+                .collect();
+            let model = slint::ModelRc::new(VecModel::from(defs));
+            dbg!(&model);
+            ui.set_definitions(model);
         })
         .unwrap();
     });
-    ui.window().set_size(PhysicalSize::new(400, 500));
-    ui.run().unwrap();
+}
+
+async fn find(query: String) -> Result<Defs> {
+    let body = get(format!(
+        "https://api.urbandictionary.com/v0/define?term={query}"
+    ))
+    .await?
+    .text()
+    .await?;
+    Ok(serde_json::from_str::<Defs>(&body)?)
 }
